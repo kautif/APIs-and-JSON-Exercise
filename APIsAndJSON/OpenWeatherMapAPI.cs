db562f6c1b3cae65770eea3b122a25c207d7cae3
Autif Kamal <kautif@gmail.com>
2023-04-18 14:54:45 -0500

    completed and tested week 4 challenges

--- APIsAndJSON/OpenWeatherMapAPI.cs
+++ APIsAndJSON/OpenWeatherMapAPI.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
 
 namespace APIsAndJSON
 {
-    internal class OpenWeatherMapAPI
+    public class OpenWeatherMapAPI
     {
+        IConfiguration config = new ConfigurationBuilder().AddJsonFile("appsettings.json", optional: true, reloadOnChange: true).Build();
+        public void getWeather(string city) {
+            var client = new HttpClient();
+            string apiKey = config.GetSection("ApiSettings:apikey_weather").Value;
+            string weatherURL = $"https://api.openweathermap.org/geo/1.0/direct?q=Houston,TX,US&appid={apiKey}";
+            var weatherResponse = client.GetStringAsync(weatherURL).Result;
+            Console.WriteLine(weatherResponse);
+        }
     }
 }
--- APIsAndJSON/Program.cs
+++ APIsAndJSON/Program.cs
@@ -10,12 +10,7 @@ public class Program
 
         var client = new HttpClient();
         var kanyeURL = "https://api.kanye.rest/";
-//        var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
-  //      var kanyeQuote = JsonObject.Parse(kanyeResponse)["quote"].ToString();
-
         var swansonURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
-//        var swansonResponse = client.GetStringAsync(swansonURL).Result;
-  //      var swansonQuote = JsonObject.Parse(swansonResponse)[0];
 
         for (int i = 0; i < 5; i++) {
             var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
@@ -27,5 +22,8 @@ public class Program
             Console.WriteLine($"Kanye: {kanyeQuote}");
             Console.WriteLine($"Swanson: {swansonQuote}");
         }
+
+        OpenWeatherMapAPI weather = new OpenWeatherMapAPI();
+        weather.getWeather("Houston", "TX");
     }
 }
