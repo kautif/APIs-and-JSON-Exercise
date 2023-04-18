46ade3d1951f8ad05026ff902547979c4dfa87ed
Autif Kamal <kautif@gmail.com>
2023-04-18 09:52:07 -0500

    looping series of 5 quotes each of Kanye and Swanson

--- APIsAndJSON/Program.cs
+++ APIsAndJSON/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace APIsAndJSON
+﻿using Newtonsoft.Json.Linq;
+using System.Text.Json.Nodes;
+
+namespace APIsAndJSON;
+
+public class Program
 {
-    public class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
+
+        var client = new HttpClient();
+        var kanyeURL = "https://api.kanye.rest/";
+//        var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+  //      var kanyeQuote = JsonObject.Parse(kanyeResponse)["quote"].ToString();
+
+        var swansonURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+//        var swansonResponse = client.GetStringAsync(swansonURL).Result;
+  //      var swansonQuote = JsonObject.Parse(swansonResponse)[0];
+
+        for (int i = 0; i < 5; i++) {
+            var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+            var kanyeQuote = JsonObject.Parse(kanyeResponse)["quote"].ToString();
+
+            var swansonResponse = client.GetStringAsync(swansonURL).Result;
+            var swansonQuote = JsonObject.Parse(swansonResponse)[0];
+
+            Console.WriteLine($"Kanye: {kanyeQuote}");
+            Console.WriteLine($"Swanson: {swansonQuote}");
         }
     }
 }
