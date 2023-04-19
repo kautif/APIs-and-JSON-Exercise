43fa451489d57f8ca69320a7c9c2d23d0eb606a4
Autif Kamal <kautif@gmail.com>
2023-04-19 11:04:24 -0500

    added api calls for quotes and weather

--- APIsAndJSON/OpenWeatherMapAPI.cs
+++ APIsAndJSON/OpenWeatherMapAPI.cs
@@ -2,11 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Nodes;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
 
 namespace APIsAndJSON
 {
-    internal class OpenWeatherMapAPI
+    public class OpenWeatherMapAPI
     {
+        public void getWeather(string city, string state)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+            var client = new HttpClient();
+            string apiKey = config.GetConnectionString("ApiKey");
+            string locationURL = $"https://api.openweathermap.org/geo/1.0/direct?q={city},{state},US&appid={apiKey}";
+            var locationResponse = client.GetStringAsync(locationURL).Result;
+            var locationJson = JsonObject.Parse(locationResponse)[0];
+            var lat = locationJson["lat"];
+            var lon = locationJson["lon"];
+
+            string weatherURL = $"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&units=imperial&appid={apiKey}";
+            var weatherResponse = client.GetStringAsync(weatherURL).Result;
+            var weatherJson = JsonObject.Parse(weatherResponse);
+            Console.WriteLine($"City: {weatherJson["name"]}");
+            Console.WriteLine($"Weather: {weatherJson["main"]["temp"]}F");
+        }
     }
 }
--- APIsAndJSON/Program.cs
+++ APIsAndJSON/Program.cs
@@ -7,17 +7,13 @@ public class Program
 {
     static void Main(string[] args)
     {
-
+        #region Exercise 1
         var client = new HttpClient();
         var kanyeURL = "https://api.kanye.rest/";
-//        var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
-  //      var kanyeQuote = JsonObject.Parse(kanyeResponse)["quote"].ToString();
-
         var swansonURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
-//        var swansonResponse = client.GetStringAsync(swansonURL).Result;
-  //      var swansonQuote = JsonObject.Parse(swansonResponse)[0];
 
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 5; i++)
+        {
             var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
             var kanyeQuote = JsonObject.Parse(kanyeResponse)["quote"].ToString();
 
@@ -27,5 +23,11 @@ public class Program
             Console.WriteLine($"Kanye: {kanyeQuote}");
             Console.WriteLine($"Swanson: {swansonQuote}");
         }
+        #endregion
+
+        var op = new OpenWeatherMapAPI();
+        op.getWeather("Hoover", "AL");
+
+
     }
 }
